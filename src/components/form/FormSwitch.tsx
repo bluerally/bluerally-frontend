dbac2032421217e7af04bf1304a9e80f70ef801c
fw-aila <aila@dunamu.dev>
2023-12-28 18:07:53 +0900

    feat: Switch, FormSwitch

---
+++ src/components/form/FormSwitch.tsx
@@ -0,0 +1,34 @@
+import { Control, Controller, FieldValues, Path } from 'react-hook-form';
+import { Switch, SwitchProps } from '@/components/common';
+
+interface Props<T extends FieldValues> extends Omit<SwitchProps, 'checked'> {
+  name: string;
+  control: Control<T>;
+}
+
+export function FormSwitch<T extends FieldValues>({
+  name,
+  control,
+  onChange,
+  ...switchProps
+}: Props<T>) {
+  return (
+    <Controller
+      control={control}
+      name={name as Path<T>}
+      render={({ field: { value, onChange: handleChange, ...rest } }) => {
+        return (
+          <Switch
+            {...rest}
+            {...switchProps}
+            checked={value}
+            onChange={(e) => {
+              handleChange(e.target.checked);
+              onChange?.(e);
+            }}
+          />
+        );
+      }}
+    />
+  );
+}
