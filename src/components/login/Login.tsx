35d6d28b0f335e56349f402312830af5900a8351
songtak <tak.song@paywatchglobal.com>
2024-01-06 17:12:32 +0900

    feat : google login 설정

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -1,3 +1,35 @@
-export const Login = () => {
-  return <>dev</>;
+import React, { useEffect, useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+
+const Login = () => {
+  // const [redirectUrl, setRedirectUrl] = useState<string>('');
+
+  const getAuthRedirectUrl = async (platform: string) => {
+    const { data, status } = await axios.get(
+      `https://bluerally.net/api/user/auth/redirect`,
+      {
+        params: { platform: platform },
+      },
+    );
+
+    status === 200
+      ? window.open(data.data.redirect_url, '_blank', 'noopener, noreferrer')
+      : console.log('error!');
+  };
+
+  return (
+    <div>
+      <div>Login</div>
+      <button
+        onClick={() => {
+          getAuthRedirectUrl('google');
+        }}
+      >
+        GoogleLogin
+      </button>
+    </div>
+  );
 };
+
+export default Login;
---
+++ src/pages/login/auth/index.tsx
@@ -0,0 +1,8 @@
+import Auth from '@/components/login/Auth';
+import { NextPageWithLayout } from '../../_app';
+
+const AuthPage: NextPageWithLayout = () => {
+  return <Auth />;
+};
+
+export default AuthPage;
