de87182716be69d0e624e735f751f43b846a5744
songtak <tak.song@paywatchglobal.com>
2024-01-15 21:01:03 +0900

    feat : 게시글 작성 폼 작성 중

---
+++ src/pages/create-party/index.tsx
@@ -0,0 +1,8 @@
+import CreateParty from '@/components/create-party/CreateParty';
+import { NextPageWithLayout } from '../_app';
+
+const CreatePartyPage: NextPageWithLayout = () => {
+  return <CreateParty />;
+};
+
+export default CreatePartyPage;
