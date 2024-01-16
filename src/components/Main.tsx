0eebffcf1179cb6c474c1429e62d954a05de1976
kimnamsun <nsunny0908@naver.com>
2024-01-16 16:15:04 +0900

    feat: 메인 리스트 컴포넌트 추가

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,9 +1,10 @@
 import { GetPartyListQuery, PartyListFilterType } from '@/@types/party/type';
 import { useGetPartyList } from '@/hooks/api/party';
-import { useRouter } from 'next/router';
+
 import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { Filter } from './main/Filter';
+import { List } from './main/List';
 
 const DEFAULT_PARAMS: GetPartyListQuery = {
   sport_id: 2,
@@ -22,7 +23,7 @@ const DEFAULT_VALUES: PartyListFilterType = {
 };
 
 const Main = () => {
-  const router = useRouter();
+  
   const [params, setParams] = useState<GetPartyListQuery>(DEFAULT_PARAMS);
 
   const form = useForm<PartyListFilterType>({
@@ -36,57 +37,12 @@ const Main = () => {
 
   const partyList = data?.data;
 
-  const goToDetail = (partyId: number) => {
-    router.push({
-      pathname: `/detail/${partyId}`,
-    });
-  };
+
 
   return (
     <>
       <Filter setParams={setParams} form={form} />
-      <>
-        {partyList?.map(
-          (
-            {
-              title,
-              sport_name,
-              price,
-              posted_date,
-              participants_info,
-              is_user_organizer,
-              organizer_profile,
-              gather_date,
-              gather_time,
-              due_date,
-              body,
-            },
-            index,
-          ) => {
-            return (
-              <div key={index}>
-                <div>{title}</div>
-                <div>{sport_name}</div>
-
-                <div>{price}</div>
-                <div>{posted_date}</div>
-                <div>{participants_info}</div>
-
-                <div>{String(is_user_organizer)}</div>
-                <div>{organizer_profile.name}</div>
-                <div>{organizer_profile.profile_picture}</div>
-
-                <div>{gather_date}</div>
-                <div>{gather_time}</div>
-
-                <div>{due_date}</div>
-                <div>{body}</div>
-                <button onClick={() => goToDetail(1)}>go to detail</button>
-              </div>
-            );
-          },
-        )}
-      </>
+      <List data={partyList} />
     </>
   );
 };
