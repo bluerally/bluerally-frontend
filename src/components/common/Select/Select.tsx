9f56d73f8771d34c95c735af3bdb3bd6397cb47a
kimnamsun <nsunny0908@naver.com>
2024-01-16 15:01:22 +0900

    feat: select, formSelect 추가

---
+++ src/components/common/Select/Select.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+export interface Option {
+  value: string;
+  label: string;
+}
+
+export interface SelectProps {
+  options: Option[];
+  onSelect: (value: string) => void;
+  selected?: string;
+}
+
+export const Select = ({ options, onSelect, selected }: SelectProps) => {
+  const handleSelectChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const value = e.target.value;
+    onSelect && onSelect(value);
+  };
+
+  return (
+    <select
+      className="px-4 py-2 leading-tight border rounded appearance-none focus:outline-none focus:shadow-outline"
+      value={selected}
+      onChange={handleSelectChange}
+    >
+      {options.map((option) => (
+        <option key={option.value} value={option.value}>
+          {option.label}
+        </option>
+      ))}
+    </select>
+  );
+};
