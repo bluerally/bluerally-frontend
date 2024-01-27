97748bd82d7aeafbdf2298744cedb283c3c094cd
kimnamsun <nsunny0908@naver.com>
2024-01-28 01:21:44 +0900

    feat: mui checkbox 추가

--- src/components/common/Checkbox/Checkbox.tsx
+++ src/components/common/Checkbox/Checkbox.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import BaseCheckbox from '@mui/material/Checkbox';
 
 export interface CheckboxProps
   extends React.InputHTMLAttributes<HTMLInputElement> {
@@ -17,41 +18,19 @@ export const Checkbox = ({
   ...rest
 }: CheckboxProps) => {
   return (
-    <label
-      className={`inline-flex items-center cursor-${
-        disabled ? 'not-allowed' : 'pointer'
-      }`}
-    >
-      <span
-        className={`
-          inline-flex justify-center items-center rounded w-4 h-4
-          ${
-            disabled && checked
-              ? 'bg-gray-300 border border-gray-200'
-              : checked
-              ? 'bg-blue-500 border border-blue-500'
-              : disabled && 'bg-gray-300 border border-gray-200'
+    <>
+      <BaseCheckbox
+        checked={checked}
+        onChange={(e) => {
+          if (readOnly) {
+            return;
           }
-        `}
-      >
-        <input
-          type="checkbox"
-          aria-hidden="true"
-          readOnly={readOnly}
-          onChange={(e) => {
-            if (readOnly) {
-              return;
-            }
-            onChange?.(e);
-          }}
-          checked={checked}
-          value={value}
-          className="hidden"
-          {...rest}
-        />
-        {checked && <span className="text-white">X</span>}
-      </span>
-      {label && <span className="pl-2 text-gray-700">{label}</span>}
-    </label>
+          onChange?.(e);
+        }}
+        readOnly={readOnly}
+        value={value}
+      />
+      {label && <span className="text-sm text-gray-700">{label}</span>}
+    </>
   );
 };
