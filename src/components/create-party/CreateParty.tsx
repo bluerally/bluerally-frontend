4a572ce92416d0e6d60ce275d1a7b658971a9f7f
songtak <missong3@naver.com>
2024-04-04 21:45:02 +0900

    feat : 입력폼 수정

--- src/components/create-party/CreateParty.tsx
+++ src/components/create-party/CreateParty.tsx
@@ -1,16 +1,23 @@
 import React, { useState, useEffect } from 'react';
 import { useForm, SubmitHandler } from 'react-hook-form';
 import DaumPostcode from 'react-daum-postcode';
-
 import _ from 'lodash';
+import { CircleDollarSign, Plus } from 'lucide-react';
 
-import { components } from '@/@types/backend';
+import { Button, Label, SearchInput } from 'bluerally-design-system';
 import { Header } from '@/components/layouts/Header';
+import { useGetSports } from '@/hooks/api/common';
+import { components } from '@/@types/backend';
+import { generateTimeOptions } from '@/utils';
 
-import { FormTextInput } from '@/components/form/FormTextInput';
-import { FormDatePicker } from '@/components/form/FormDatePicker';
-import { FormSwitch } from '@/components/form/FormSwitch';
-import { FormSelect } from '@/components/form/FormSelect';
+import { FormButtonGroup } from '../form/FormButtonGroup';
+import { FormDatePicker } from '../form/FormDatePicker';
+
+/** @todo 숫자만 입력 가능 */
+/** @todo 숫자만 입력 가능일때 3째자리 마다 콤마 설정 */
+import { FormTextInput } from '../form/FormTextInput';
+import { FormSelect } from '../form/FormSelect';
+import { FormTextArea } from '../form/FormTextArea';
 
 declare global {
   interface Window {
@@ -25,13 +32,19 @@ type SportType = {
 };
 
 const CreateParty = () => {
+  const { data: sportsData } = useGetSports();
+
   const [isOpenPostcode, setIsOpenPostcode] = useState<boolean>(false);
   /** 선택한 도로명 주소 */
   const [roadAddress, setRoadAddress] = useState<string>('');
   /** 위도/경도 [lat(y), lng(x)] */
   const [geoPoint, setGeoPoint] = useState<String[]>(['']);
 
+  /** 추가정보 */
+  const [isOpenNotice, setIsOpenNotice] = useState<boolean>(false);
+
   const {
+    control,
     register,
     handleSubmit,
     setValue,
@@ -50,7 +63,9 @@ const CreateParty = () => {
 
   const watchAll = watch();
 
-  console.log('watchAll', watchAll);
+  // console.log('watchAll', watchAll);
+  console.log('watchAll.latitude', watchAll.latitude);
+  console.log('watchAll.longitude', watchAll.longitude);
 
   /** ========================================================================================== */
 
@@ -63,6 +78,14 @@ const CreateParty = () => {
     { id: 3, key: 'fishing', value: '낚시' },
     { id: 4, key: 'swim', value: '수영' },
   ];
+
+  const sports = sportsData?.data ?? [];
+
+  let participantLimitList: any[] = [];
+
+  for (let i = 2; i <= 30; i++) {
+    participantLimitList.push({ id: i, name: `${i}명` });
+  }
 
   /** ========================================================================================== */
 
@@ -77,6 +100,11 @@ const CreateParty = () => {
   };
 
   /** ========================================================================================== */
+
+  /** 주소 검색창 열기 */
+  const handleClickOpenPostCode = () => {
+    //
+  };
 
   /** 게시버튼 클릭 */
   const handleClickApply = () => {
@@ -96,6 +124,8 @@ const CreateParty = () => {
   };
   /** ========================================================================================== */
 
+  console.log('roadAddress', roadAddress);
+
   useEffect(() => {
     const kakaoMapScript = document.createElement('script');
     kakaoMapScript.async = false;
@@ -107,6 +137,7 @@ const CreateParty = () => {
         var container = document.getElementById('map');
         var options = {
           center: new window.kakao.maps.LatLng(33.450701, 126.570667),
+          // center: new window.kakao.maps.LatLng(33.450701, 126.570667),
           level: 3,
         };
 
@@ -144,99 +175,136 @@ const CreateParty = () => {
     <div>
       <Header leftTpye="back" title="글쓰기" customButton={applyButton} />
       <form onSubmit={handleSubmit(handleCreateParty)}>
-        <div id="map" style={{ width: '300px', height: '300px' }}></div>
         <div>
           <div>종류</div>
-          <div>
-            {/* {...register('sport_id', { required: true })} */}
-            {sportsList.map((item: SportType) => (
-              <button
-                key={item.id}
-                onClick={() => {
-                  setValue('sport_id', item.id, { shouldValidate: true });
-                }}
-              >
-                {item.value}
-              </button>
-            ))}
-          </div>
-
-          {/* <input
-          type="number"
-          {...register('sport_id', { required: true })}
-          placeholder="Sport ID"
-        /> */}
-
-          {/* <select
-          value={watchAll.sport_id}
-          {...register('sport_id', { required: true })}
-        >
-          <option value="">스포츠를 선택하세요</option>
-          {sportsList.map((sport) => (
-            <option
-              key={sport.id}
-              value={sport.id}
-              onClick={() => {
-                setValue('sport_id', sport.id, { shouldValidate: true });
-              }}
-            >
-              {sport.value}
-            </option>
-          ))}
-        </select> */}
-
-          {/* {errors.sport_id && <span>This field is required</span>} */}
+          <FormButtonGroup control={control} name="sports" options={sports} />
         </div>
 
-        <div>
-          마감 날짜
+        <div className="pb-8">
+          <Label>마감 날짜</Label>
+          <div className="pt-1.5">
+            <FormDatePicker control={control} name="due_at" width="100%" />
+          </div>
+          {/* 마감 날짜
           <input
             {...register('due_at', { required: true })}
             placeholder="Due At"
           />
-          {errors.due_at && <span>This field is required</span>}
+          {errors.due_at && <span>This field is required</span>} */}
+        </div>
+        <div>
+          <Label>모임 시작 시간</Label>
+          <div className="pt-1.5">
+            <FormSelect
+              control={control}
+              name="gather_at"
+              width="100%"
+              options={generateTimeOptions()}
+              optionMaxHeight={200}
+            />
+          </div>
+          {/* <input
+            {...register('gather_at', { required: true })}
+            placeholder="Gather At"
+          />
+          {errors.gather_at && <span>This field is required</span>} */}
         </div>
 
         <div>
-          제목 : 50자
-          <input
+          <Label>금액</Label>
+          <div className="pt-1.5">
+            <FormTextInput
+              control={control}
+              name="participant_cost"
+              placeholder="1000원"
+              status={errors.participant_cost ? 'error' : 'default'}
+              statusMessage={errors.participant_cost?.message}
+              startIcon={<CircleDollarSign size={18} color="#A1A1AA" />}
+            />
+          </div>
+          {/* <input
             {...register('title', { required: true })}
             placeholder="Title"
             maxLength={50}
           />
-          {errors.title && <span>This field is required</span>}
+          {errors.title && <span>This field is required</span>} */}
+        </div>
+        <div>
+          <Label>인원 입력</Label>
+          <FormButtonGroup
+            control={control}
+            name="participant_limit"
+            options={participantLimitList}
+          />
         </div>
 
         <div>
-          내용 : 500자
-          <textarea
+          <div className="pt-1.5">
+            <FormTextInput
+              control={control}
+              name="title"
+              placeholder="제목을 입력하세요"
+              status={errors.title ? 'error' : 'default'}
+              statusMessage={errors.title?.message}
+            />
+          </div>
+          {/* <input
+            {...register('title', { required: true })}
+            placeholder="Title"
+            maxLength={50}
+          />
+          {errors.title && <span>This field is required</span>} */}
+        </div>
+
+        <div>
+          <FormTextArea
+            control={control}
+            name="body"
+            placeholder="내용을 입력해주세요"
+            status={errors.body ? 'error' : 'default'}
+            statusMessage={errors.body?.message}
+          />
+          {/* <textarea
             {...register('body', { required: true })}
             maxLength={500}
             placeholder="Body"
           />
-          {errors.body && <span>This field is required</span>}
+          {errors.body && <span>This field is required</span>} */}
         </div>
-
         <div>
-          모임 시간
-          <input
-            {...register('gather_at', { required: true })}
-            placeholder="Gather At"
-          />
-          {errors.gather_at && <span>This field is required</span>}
+          {/* 카카오맵 */}
+          {/* {!_.isEmpty(watchAll.latitude) && !_.isEmpty(watchAll.longitude) && (
+            <div id="map" style={{ width: '300px', height: '300px' }}></div>
+          )} */}
+          <div
+            id="map"
+            style={{
+              width: '300px',
+              height: '300px',
+              display: `${
+                (_.isUndefined(watchAll.latitude) &&
+                  _.isUndefined(watchAll.longitude) &&
+                  'none') ||
+                (!_.isUndefined(watchAll.latitude) &&
+                  !_.isUndefined(watchAll.longitude) &&
+                  'content')
+              }`,
+            }}
+          ></div>
+          {/* {!_.isUndefined(watchAll.latitude) &&
+            !_.isUndefined(watchAll.longitude) && (
+              <div
+                id="map"
+                style={{
+                  width: '300px',
+                  height: '300px',
+                }}
+              ></div>
+            )} */}
         </div>
 
         <div>
           장소
-          <input
-            {...register('place_name', { required: true })}
-            placeholder="Place Name"
-          />
-          {errors.place_name && <span>This field is required</span>}
-        </div>
-
-        <div>
-          상세 주소
           <input
             {...register('address', { required: true })}
             placeholder="Address"
@@ -245,7 +313,6 @@ const CreateParty = () => {
             }}
             value={watchAll.address}
           />
-          {errors.address && <span>This field is required</span>}
         </div>
         {isOpenPostcode && (
           <DaumPostcode
@@ -255,25 +322,10 @@ const CreateParty = () => {
           />
         )}
 
-        <div>
-          인원 입력 (드롭다운x)
-          <input
-            type="number"
-            {...register('participant_limit')}
-            placeholder="Participant Limit"
-          />
+        <div onClick={() => {}}>
+          <Plus />
+          <Label>추가정보</Label>
         </div>
-        <div>
-          금액
-          <input
-            type="number"
-            {...register('participant_cost')}
-            placeholder="Participant Cost"
-          />
-        </div>
-        <div style={{ height: 1400 }}></div>
-
-        <button type="submit">등록</button>
       </form>
     </div>
   );
