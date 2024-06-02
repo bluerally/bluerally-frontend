847473b9eea4675d6101a866f44fa3ed2be7fe80
kimnamsun <nsunny0908@naver.com>
2024-06-02 14:14:22 +0900

    feat: 마이페이지List부분 스크롤 수정

--- src/components/profile/MyProfileComponent.tsx
+++ src/components/profile/MyProfileComponent.tsx
@@ -1,13 +1,13 @@
 import { Tabs } from 'bluerally-design-system';
 import { useCallback, useState } from 'react';
-import { List } from '../main/List';
+import { List } from '@/components/main/List';
 import {
   useGetPartyMeOrganized,
   useGetPartyMeParticipated,
   useGetUserMe,
 } from '@/hooks/api/user';
-import { Profile } from '../common/Profile';
-import { Header } from '../layouts/Header';
+import { Profile } from '@/components/common/Profile';
+import { Header } from '@/components/layouts/Header';
 import { ChevronLeft } from 'lucide-react';
 
 export const MyProfileComponent = () => {
@@ -37,35 +37,42 @@ export const MyProfileComponent = () => {
         }
         center={<>마이페이지</>}
       />
-      <div className="p-5">
-        <Profile userId={currentUser?.id} isMyProfile={true} />
+      <div className="flex flex-col h-screen">
+        <div className="flex-shrink-0 p-5">
+          <Profile userId={currentUser?.id} isMyProfile={true} />
+        </div>
+        <div className="flex-shrink-0">
+          <Tabs
+            onTabChange={handleTabChange}
+            selected={selected}
+            items={[
+              {
+                label: `참여한 모임`,
+                value: 'participationParty',
+                content: null, // content를 여기서 비워둠
+              },
+              {
+                label: `주최한 모임`,
+                value: 'selfOrganizationParty',
+                content: null, // content를 여기서 비워둠
+              },
+            ]}
+          />
+        </div>
+        <div className="flex-grow overflow-y-auto bg-g-1">
+          {selected === 'participationParty' ? (
+            <List
+              data={partyMeParticipatedData?.data}
+              noDataMessage="참여한 모임이 없어요"
+            />
+          ) : (
+            <List
+              data={partyMeOrganizationData?.data}
+              noDataMessage="주최한 모임이 없어요"
+            />
+          )}
+        </div>
       </div>
-      <Tabs
-        onTabChange={handleTabChange}
-        selected={selected}
-        items={[
-          {
-            label: `참여한 모임`,
-            value: 'participationParty',
-            content: (
-              <List
-                data={partyMeParticipatedData?.data}
-                noDataMessage="참여한 모임이 없어요"
-              />
-            ),
-          },
-          {
-            label: `주최한 모임`,
-            value: 'selfOrganizationParty',
-            content: (
-              <List
-                data={partyMeOrganizationData?.data}
-                noDataMessage="주최한 모임이 없어요"
-              />
-            ),
-          },
-        ]}
-      />
     </>
   );
 };
