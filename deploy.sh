5313832fd6d91ac36edd99c31fb5896d0a244f0f
kimnamsun <nsunny0908@naver.com>
2024-07-14 17:30:52 +0900

    ci: sudo 추가

--- deploy.sh
+++ deploy.sh
@@ -4,20 +4,20 @@ REPOSITORY=/home/ec2-user/fe-app
 # 디렉토리 이동
 cd $REPOSITORY
 
-# Node.js 및 npm 설치 확인
+# Node.js 설치 확인 및 설치
 if ! command -v node &> /dev/null; then
-  curl -sL https://rpm.nodesource.com/setup_16.x | bash -
-  yum install -y nodejs
+  curl -sL https://rpm.nodesource.com/setup_16.x | sudo bash -
+  sudo yum install -y nodejs
 fi
 
 # npm 설치 확인 및 경로 설정
 if ! command -v npm &> /dev/null; then
-  curl -L https://www.npmjs.com/install.sh | sh
+  sudo yum install -y npm
 fi
 
 # pnpm 설치 확인 및 설치
 if ! command -v pnpm &> /dev/null; then
-  npm install -g pnpm
+  sudo npm install -g pnpm
   export PATH="$HOME/.npm-global/bin:$PATH"
   source ~/.bashrc
 fi
