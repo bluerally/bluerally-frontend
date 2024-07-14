229964d82f53245ce67f1703c362e9ac32506c8c
kimnamsun <nsunny0908@naver.com>
2024-07-14 15:26:04 +0900

    ci: deploy.sh 파일 내에서 log 찍도록 수정

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,3 @@
-REPOSITORY=/home/ubuntu/deploy
-
-cd $REPOSITORY
-
-yarn deploy
+DEPLOY_LOG="/home/ec2-user/deploy-fe.log"
+echo "" > $DEPLOY_LOG
+echo "$(date '+%Y-%m-%d %H:%M:%S') > 배포 시작" >> $DEPLOY_LOG
