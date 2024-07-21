8d82fc735cc614d1d35e6cf1c8909fcb5445daff
kimnamsun <nsunny0908@naver.com>
2024-07-22 01:07:07 +0900

    ci: deploy.sh에서 docker pull하는 코드 추가

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,14 @@
+#!/bin/bash
+
 # REPOSITORY 변수 설정
 REPOSITORY=/home/ec2-user/fe-app
 
-# 디렉토리 이동
-cd $REPOSITORY
+# Docker Hub에서 최신 이미지 가져오기
+docker pull bluerally/blue-rally-fe-build.zip
 
-npm i
-npm run deploy
+# 기존 컨테이너 중지 및 제거
+docker stop bluerally-fe || true
+docker rm bluerally-fe || true
+
+# Docker 컨테이너 실행
+docker run -d -p 3000:3000 --name bluerally-fe bluerally/blue-rally-fe-build.zip
