81627c1d17c4a604c5338c33c4183ac922dc1594
kimnamsun <nsunny0908@naver.com>
2024-07-22 23:42:57 +0900

    ci: deploy.sh 수정

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,14 @@
-#!/bin/bash
+# REPOSITORY 변수 설정
+REPOSITORY=/home/ec2-user/fe-app
+
+# 디렉토리 이동
+cd $REPOSITORY
 
 DEPLOY_LOG="/home/ec2-user/deploy-fe.log"
 # 배포 로그 파일 초기화
 echo "" > $DEPLOY_LOG
+
+echo "$(date '+%Y-%m-%d %H:%M:%S') > 배포 시작" >> $DEPLOY_LOG
 
 # Docker 이미지 다운로드
 echo "$(date '+%Y-%m-%d %H:%M:%S') > Pulling Docker image" >> $DEPLOY_LOG
