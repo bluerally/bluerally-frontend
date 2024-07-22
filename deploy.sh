2d5229625b0ce1204b910b600e887e35b3e1159c
kimnamsun <nsunny0908@naver.com>
2024-07-23 00:03:10 +0900

    ci: deploy.sh 수정

--- deploy.sh
+++ deploy.sh
@@ -14,11 +14,6 @@ echo "$(date '+%Y-%m-%d %H:%M:%S') > 배포 시작" >> $DEPLOY_LOG
 echo "$(date '+%Y-%m-%d %H:%M:%S') > Pulling Docker image" >> $DEPLOY_LOG
 sudo docker pull bluerally/blue-rally-fe:latest 2>&1 | tee -a $DEPLOY_LOG
 
-# 기존 컨테이너 중지 및 제거
-echo "$(date '+%Y-%m-%d %H:%M:%S') > Stopping and removing existing container" >> $DEPLOY_LOG
-sudo docker stop bluerally-fe || true
-sudo docker rm bluerally-fe || true
-
 # Docker 컨테이너 실행
 echo "$(date '+%Y-%m-%d %H:%M:%S') > Running new Docker container" >> $DEPLOY_LOG
-sudo docker run -d -p 3000:3000 --name bluerally-fe bluerally/blue-rally-fe:latest 2>&1 | tee -a $DEPLOY_LOG
+sudo docker run -d -p 3000:3000 --name bluerally-fe bluerally/bluerally-fe:latest 2>&1 | tee -a $DEPLOY_LOG
