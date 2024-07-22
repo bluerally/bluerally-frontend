721ea9278cb73931ac5a141dd2c853c1cc415974
kimnamsun <nsunny0908@naver.com>
2024-07-22 21:33:38 +0900

    ci: deploy.sh에서 sudo 명령어 추가

--- deploy.sh
+++ deploy.sh
@@ -4,11 +4,11 @@
 REPOSITORY=/home/ec2-user/fe-app
 
 # Docker Hub에서 최신 이미지 가져오기
-docker pull bluerally/blue-rally-fe-build.zip
+sudo docker pull bluerally/blue-rally-fe-build.zip
 
 # 기존 컨테이너 중지 및 제거
-docker stop bluerally-fe || true
-docker rm bluerally-fe || true
+sudo docker stop bluerally-fe || true
+sudo docker rm bluerally-fe || true
 
 # Docker 컨테이너 실행
-docker run -d -p 3000:3000 --name bluerally-fe bluerally/blue-rally-fe-build.zip
+sudo docker run -d -p 3000:3000 --name bluerally-fe bluerally/blue-rally-fe-build.zip
