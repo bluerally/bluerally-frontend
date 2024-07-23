8815fbaa14e0c3e09b34d2a43d1f71a40456b970
myeongjin yang <78643271+audwls624@users.noreply.github.com>
2024-07-24 00:14:07 +0900

    deploy 스크립트 수정(network 연결 추가)

--- deploy.sh
+++ deploy.sh
@@ -21,6 +21,11 @@ sudo docker pull bluerally/blue-rally-fe:$IMAGE_TAG 2>&1 | tee -a $DEPLOY_LOG
 echo "$(date '+%Y-%m-%d %H:%M:%S') > Running new Docker container" >> $DEPLOY_LOG
 sudo docker run -d -p 3000:3000 --name bluerally-fe --network bluerally-network bluerally/bluerally-fe:$IMAGE_TAG 2>&1 | tee -a $DEPLOY_LOG
 
+# 네트워크 연결 확인 및 필요시 연결
+if ! sudo docker network inspect bluerally-network | grep -q "bluerally-fe"; then
+    sudo docker network connect bluerally-network bluerally-fe
+fi
+
 # 오래된 Docker 이미지 정리
 echo "$(date '+%Y-%m-%d %H:%M:%S') > Cleaning up old Docker images" >> $DEPLOY_LOG
 sudo docker image prune -a -f 2>&1 | tee -a $DEPLOY_LOG
