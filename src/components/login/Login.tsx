31ea32504848e69d58335facafb9dacfe466901a
kimnamsun <nsunny0908@naver.com>
2024-11-09 14:00:30 +0900

    fix: 로그인페이지 이미지 수정

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -16,51 +16,49 @@ export const Login = () => {
   };
 
   return (
-    <>
-      <div className="flex flex-col items-center w-full h-full bg-center bg-cover bg-[url('/images/blue_background.svg')]">
-        <div className="self-start p-10 cursor-pointer">
-          <ChevronLeft onClick={() => router.push('/')} />
-        </div>
-        <div className="flex flex-col items-center justify-center flex-grow">
-          <Image
-            src={`/images/logo_white.svg`}
-            alt="buooy"
-            width={160}
-            height={56}
-            priority
-            className="mb-5"
-          />
-        </div>
-        <div className="flex flex-col items-center w-full gap-5 mt-auto">
-          <Image
-            src={`/images/kakao.svg`}
-            alt="kakao login"
-            width={460}
-            height={54}
-            priority
-            onClick={() => handleClickLoginButton('kakao')}
-            className="cursor-pointer w-full max-w-[460px]"
-          />
-          <Image
-            src={`/images/google.svg`}
-            alt="google login"
-            width={460}
-            height={54}
-            priority
-            onClick={() => handleClickLoginButton('google')}
-            className="cursor-pointer w-full max-w-[460px]"
-          />
-          <Image
-            src={`/images/naver.svg`}
-            alt="naver login"
-            width={460}
-            height={54}
-            priority
-            onClick={() => handleClickLoginButton('naver')}
-            className="cursor-pointer w-full max-w-[460px]"
-          />
-        </div>
+    <div className="p-5 w-full h-full bg-center bg-cover bg-[url('/images/login_background.svg')] flex flex-col">
+      <div className="self-start mb-5 cursor-pointer">
+        <ChevronLeft onClick={() => router.push('/')} />
       </div>
-    </>
+      <div className="flex flex-col items-center justify-center flex-grow">
+        <Image
+          src={`/images/logo_white.svg`}
+          alt="buooy"
+          width={160}
+          height={56}
+          priority
+          className="mb-5"
+        />
+      </div>
+      <div className="flex flex-col items-center w-full gap-5 pb-10">
+        <Image
+          src={`/images/kakao.svg`}
+          alt="kakao login"
+          width={460}
+          height={54}
+          priority
+          onClick={() => handleClickLoginButton('kakao')}
+          className="cursor-pointer w-full max-w-[460px]"
+        />
+        <Image
+          src={`/images/google.svg`}
+          alt="google login"
+          width={460}
+          height={54}
+          priority
+          onClick={() => handleClickLoginButton('google')}
+          className="cursor-pointer w-full max-w-[460px]"
+        />
+        <Image
+          src={`/images/naver.svg`}
+          alt="naver login"
+          width={460}
+          height={54}
+          priority
+          onClick={() => handleClickLoginButton('naver')}
+          className="cursor-pointer w-full max-w-[460px]"
+        />
+      </div>
+    </div>
   );
 };
