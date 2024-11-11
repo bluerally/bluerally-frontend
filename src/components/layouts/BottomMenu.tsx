c540031277f8272a3bbd233c28911d14d3c2e66d
kimnamsun <nsunny0908@naver.com>
2024-11-11 23:09:10 +0900

    fix: bottomMenu 글자 클릭시에도 cursor 적용 및 onClick 적용

--- src/components/layouts/BottomMenu.tsx
+++ src/components/layouts/BottomMenu.tsx
@@ -13,40 +13,30 @@ export const BottomMenu = () => {
     <div className="sticky bottom-0 w-full h-[56px] px-9 py-4 bg-white text-center border-t border-gray-100">
       <div className="flex items-center justify-around h-full max-w-screen-lg mx-auto">
         <div
-          className={`flex flex-col items-center gap-1 ${isCurrentMenu('/')}`}
+          className={`flex flex-col items-center gap-1 cursor-pointer ${isCurrentMenu(
+            '/',
+          )}`}
+          onClick={() => router.push(`/`)}
         >
-          <Home
-            size={22}
-            strokeWidth={1.5}
-            className="cursor-pointer"
-            onClick={() => router.push(`/`)}
-          />
+          <Home size={22} strokeWidth={1.5} />
           <span className="text-sm">홈</span>
         </div>
         <div
-          className={`flex flex-col items-center gap-1 ${isCurrentMenu(
+          className={`flex flex-col items-center gap-1 cursor-pointer ${isCurrentMenu(
             '/create-party',
           )}`}
+          onClick={() => router.push(`/create-party`)}
         >
-          <PenSquare
-            size={22}
-            strokeWidth={1.5}
-            onClick={() => router.push(`/create-party`)}
-            className="cursor-pointer"
-          />
+          <PenSquare size={22} strokeWidth={1.5} />
           <span className="text-sm">모임개설</span>
         </div>
         <div
-          className={`flex flex-col items-center gap-1 ${isCurrentMenu(
+          className={`flex flex-col items-center gap-1 cursor-pointer ${isCurrentMenu(
             '/profile',
           )}`}
+          onClick={() => router.push(`/profile`)}
         >
-          <UserRound
-            size={22}
-            strokeWidth={1.5}
-            onClick={() => router.push(`/profile`)}
-            className="cursor-pointer"
-          />
+          <UserRound size={22} strokeWidth={1.5} />
           <span className="text-sm">마이페이지</span>
         </div>
       </div>
