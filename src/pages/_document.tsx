a0a942d78b235349e80566a64a521886a0d57b1e
kimnamsun <nsunny0908@naver.com>
2024-11-17 17:34:22 +0900

    fix: 컬러 변경 및 appkey 수정

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,5 @@
 import Document, { Head, Html, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 export default class CustomDocument extends Document {
   render() {
@@ -7,6 +8,10 @@ export default class CustomDocument extends Document {
         <Head>
           <meta charSet="utf-8" />
           <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+          <Script
+            type="text/javascript"
+            src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_MAP_KEY}&libraries=services`}
+          />
         </Head>
         <body className="bg-g-100">
           <Main />
