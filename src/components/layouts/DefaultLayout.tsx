8c2bd99c595b4a84ed2958a921194c26dd89e7c4
kimnamsun <nsunny0908@naver.com>
2024-12-22 23:38:20 +0900

    fix: layout min-height 지정

--- src/components/layouts/DefaultLayout.tsx
+++ src/components/layouts/DefaultLayout.tsx
@@ -2,6 +2,13 @@ import { PropsWithChildren } from 'react';
 
 export const DefaultLayout = ({ children }: PropsWithChildren) => {
   return (
-    <main className=" mx-auto bg-g-0 w-full max-w-[600px]">{children}</main>
+    <main
+      className="h-screen mx-auto bg-g-0 w-full max-w-[600px]"
+      style={{
+        minHeight: '100vh - 112px',
+      }}
+    >
+      {children}
+    </main>
   );
 };
